8c06c88c72e5071add57c91a20aa3621196a070d
ruslan.dasaev <ruslan.dasaev@myoffice.team>
2023-08-16 17:24:24 +0300

    added types for CardsField comp; removed extra react element

--- src/components/CardsField.tsx
+++ src/components/CardsField.tsx
@@ -1,26 +1,40 @@
-
+import { ICard, Point } from '../types';
 import { Card } from './Card';
-export function CardsField({canvasPosition, scale, cards, onRemoveHandler, changeCardsArrayCb}) {
+
+interface ICardsFieldProps {
+  canvasPosition: Point;
+  scale: number;
+  cards: ICard[];
+  onRemoveHandler: (id: string) => void;
+  changeCardsArrayCb: (card: ICard) => void;
+}
+
+export function CardsField({
+  canvasPosition,
+  scale,
+  cards,
+  onRemoveHandler,
+  changeCardsArrayCb,
+}: ICardsFieldProps) {
   return (
-    <>
-      <div className="cards-wrapper"
+    <div
+      className="cards-wrapper"
       style={{
-        transformOrigin: "0px 0px",
+        transformOrigin: '0px 0px',
         transform: `translate(${canvasPosition.x}px, ${canvasPosition.y}px) scale(${scale})`,
       }}
-      >
-        {cards.map((cardItem) => {
-          return (
-            <Card
-              key={cardItem.id}
-              cardData={cardItem}
-              onRemoveCard={onRemoveHandler}
-              changeCardsArray={changeCardsArrayCb}
-              scale={scale}
-            />
-          );
-        })}
-      </div>
-    </>
+    >
+      {cards.map((cardItem) => {
+        return (
+          <Card
+            key={cardItem.id}
+            cardData={cardItem}
+            onRemoveCard={onRemoveHandler}
+            changeCardsArray={changeCardsArrayCb}
+            scale={scale}
+          />
+        );
+      })}
+    </div>
   );
 }
